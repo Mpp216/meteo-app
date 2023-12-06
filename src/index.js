a6206e2e46433ba9e234f8d790d1fa51d8c7b81a
Mpp216 <mariappereira96@gmail.com>
2023-12-06 15:50:49 +0100

    Added id in html and search functionality with API

--- src/index.js
+++ src/index.js
@@ -1,9 +1,28 @@
+function updateWheather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+  console.log(response.data);
+}
+
+function searchCity(city) {
+  let apiKey = "8b274fd4b3b0of2fe273a1bf90eetcd2";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(updateWheather);
+}
+
 function search(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-input");
-  let cityElement = document.querySelector("#current-city");
-  cityElement.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", search);
+
+searchCity("Amsterdam");
